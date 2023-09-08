35e41f582ff405c214f41b172fb4a0baa3fddbd7
AvazMuzafarov <avazmuzafarov72744968@gmail.com>
2023-09-08 04:32:23 +0300

    first commit

---
+++ orelreshka.py
@@ -0,0 +1,120 @@
+from livewires import games, color
+import random
+
+
+games.init(screen_height=480, screen_width=640, fps=50)
+
+the_sky = games.load_image("sky.jpg", transparent=False)
+games.screen.background = the_sky
+
+class Flip(games.Animation):
+    flipchik = ["orelre.bmp",
+                "reshkare.bmp"]
+    def __init__(self):
+        super(Flip, self).__init__(images=Flip.flipchik,
+                                   x=games.screen.width / 2,
+                                   y=games.screen.height / 2,
+                                   n_repeats=10,
+                                   repeat_interval=25)
+
+class Changer(games.Sprite):
+
+    def change(self):
+        y = random.randrange(2)
+        if y == 0:
+            orel = Orel()
+            games.screen.add(orel)
+            orel.update()
+
+        if y == 1:
+            reshka = Reshka()
+            games.screen.add(reshka)
+            reshka.update()
+
+
+        new_flip = Flip()
+        games.screen.add(new_flip)
+
+
+
+
+
+class F_button (games.Text):
+    f_delay = 1000
+    def __init__(self):
+        super(F_button, self).__init__(value="Нажмите F, чтобы подбросить монету.",
+                                       size=30,
+                                       color=color.black,
+                                       right = games.screen.width - 15,
+                                       y = 20)
+        self.f_wait = 0
+
+    def update(self):
+        if self.f_wait > 0:
+            self.f_wait -= 1
+
+        if games.keyboard.is_pressed(games.K_f) and self.f_wait == 0:
+            self.f_wait += F_button.f_delay
+            retro.change()
+
+
+class Orel(Changer):
+    orel = games.load_image("orelre.bmp")
+    def __init__(self):
+        super(Orel, self).__init__(image=Orel.orel,
+                                     x=games.screen.width / 2,
+                                     y=games.screen.height / 2,
+                                     angle=0)
+        self.time = 1000
+    def ddd(self):
+        self.destroy()
+
+    def update(self):
+        self.time -=1
+        if self.time <1:
+            self.ddd()
+        if self.time <500:
+            hint1 = Hint("Орел")
+            games.screen.add(hint1)
+retro = Orel()
+class Reshka(Changer):
+    reshka = games.load_image("reshkare.bmp")
+    def __init__(self):
+        super(Reshka, self).__init__(image=Reshka.reshka,
+                                   x = games.screen.width/2,
+                                   y = games.screen.height/2,
+                                   angle=0)
+        self.time = 1000
+
+    def ddd(self):
+        self.destroy()
+
+    def update(self):
+        self.time -= 1
+        if self.time < 1:
+            self.ddd()
+        if self.time < 500:
+            hint2 = Hint("Решка")
+            games.screen.add(hint2)
+
+class Hint(games.Message):
+    def __init__(self, value):
+        super(Hint, self).__init__(value = value,
+                                   size = 40,
+                                   color = color.red,
+                                   x = games.screen.width/2,
+                                   bottom = games.screen.height - 20,
+                                   lifetime = 50)
+
+
+
+
+def main():
+    f_b = F_button()
+    games.screen.add(f_b)
+    games.screen.mainloop()
+
+
+main()
+
+
